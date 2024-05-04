e1d1bcdeee43ef7c67c51951736dcc90bb755b17
isaac-gullickson <isaac.gullickson@smartthings.com>
2024-05-04 11:47:11 -0500

    iterate on schedule generation

--- tmag/internal/agenda.go
+++ tmag/internal/agenda.go
@@ -19,31 +19,58 @@ type Agenda struct {
 	Timer             string `yaml:"timer,omitempty"`
 }
 
+var supportedRoles []string = []string{
+	"toastmaster",
+	"speaker",
+	"speaker 1",
+	"speaker 2",
+	"tabletopics master",
+	"general evaluator",
+	"evaluator 1",
+	"evaluator 2",
+	"grammarian",
+	"ah counter",
+	"timer",
+}
+
 func NewAgenda() Agenda {
 	return Agenda{}
 }
 
-func RandomAgenda(roles []string, members []string) (*Agenda, error) {
-	if len(roles) != len(members) {
-		return nil, fmt.Errorf("number of roles, %d, did not equal number of members, %d", len(roles), len(members))
+func (a *Agenda) GetMemberForRole(role string) (string, error) {
+	roleLower := strings.ReplaceAll(strings.ToLower(role), " ", "")
+	var member string
+
+	switch roleLower {
+	case "toastmaster":
+		member = a.Toastmaster
+	case "speaker":
+		member = a.Speaker1
+	case "speaker1":
+		member = a.Speaker1
+	case "speaker2":
+		member = a.Speaker2
+	case "tabletopicsmaster":
+		member = a.TableTopicsMaster
+	case "generalevaluator":
+		member = a.GeneralEvaluator
+	case "evaluator":
+		member = a.Evaluator1
+	case "evaluator1":
+		member = a.Evaluator1
+	case "evaluator2":
+		member = a.Evaluator2
+	case "grammarian":
+		member = a.Grammarian
+	case "ahcounter":
+		member = a.AhCounter
+	case "timer":
+		member = a.Timer
+	default:
+		return "", fmt.Errorf("unexpected role: %s. supported roles: %s", role, strings.Join(supportedRoles, ", "))
 	}
 
-	shuffle(members)
-
-	var a = NewAgenda()
-
-	for i, v := range roles {
-		err := a.AddAssignment(v, members[i])
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return &a, nil
-}
-
-func shuffle(a []string) {
-	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+	return member, nil
 }
 
 func (a *Agenda) AddAssignment(role string, member string) error {
@@ -75,19 +102,6 @@ func (a *Agenda) AddAssignment(role string, member string) error {
 	case "timer":
 		a.Timer = member
 	default:
-		supportedRoles := []string{
-			"toastmaster",
-			"speaker",
-			"speaker 1",
-			"speaker 2",
-			"tabletopics master",
-			"general evaluator",
-			"evaluator 1",
-			"evaluator 2",
-			"grammarian",
-			"ah counter",
-			"timer",
-		}
 		return fmt.Errorf("unexpected role: %s. supported roles: %s", role, strings.Join(supportedRoles, ", "))
 	}
 
@@ -103,4 +117,27 @@ func (a *Agenda) RepeatsRole(other Agenda) bool {
 		a.Grammarian == other.Grammarian ||
 		a.AhCounter == other.AhCounter ||
 		a.Timer == other.Timer
+}
+
+func Shuffle(a []string) {
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+}
+
+func RandomAgenda(roles []string, members []string) (*Agenda, error) {
+	if len(roles) != len(members) {
+		return nil, fmt.Errorf("number of roles, %d, did not equal number of members, %d", len(roles), len(members))
+	}
+
+	Shuffle(members)
+
+	var a = NewAgenda()
+
+	for i, v := range roles {
+		err := a.AddAssignment(v, members[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &a, nil
 }
